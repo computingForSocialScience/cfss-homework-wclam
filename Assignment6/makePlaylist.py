8c7daa14347afd74074556f36bb9bcc4b0a14e2d
wclam <wclam@uchicago.edu>
2015-02-24 12:35:54 -0600

    updated makePlaylist

--- Assignment6/makePlaylist.py
+++ Assignment6/makePlaylist.py
@@ -6,6 +6,15 @@ from analyzeNetworks import *
 from fetchArtist import *
 from fetchAlbums import *
 
+def fetchTrackNames(album_id):
+	url = 'https://api.spotify.com/v1/albums/' + album_id + '/tracks'
+   	req = requests.get(url)
+   	data = req.json()
+   	track_list = []
+   	for track in data['items']:
+   		track_list.append(track['name'])
+   	return track_list
+
 if __name__ == '__main__':
 	artist_names = sys.argv[1:]
 	print 'input artists are', artist_names
@@ -40,32 +49,25 @@ if __name__ == '__main__':
 #convert to networkx
 	g = pandasToNetworkX2(edgeList)
 #collect random sample
-	random_artists = []
-	i = 0
-	while i != 30:
-		random_artist = randomCentralNode(g)
-		random_artists.append(random_artist)
-		i += 1
+	k = 0
+	playlist = []
+	while k != 30:
+		artist_dict = {}
+		artist_dict['artist_name'] = fetchArtistInfo(randomCentralNode(g))['name']
+		album_id = np.random.choice(fetchAlbumIds(fetchArtistId(artist_dict['artist_name'])))
+		artist_dict['album_name'] = fetchAlbumInfo(album_id)['name']
+		artist_dict['track_name'] = np.random.choice(fetchTrackNames(album_id))
+		playlist.append(artist_dict)
+		k += 1
 
 #playlist
-	"""f = open("playlist.csv", "w", encoding = 'utf-8')
-f.write('"%s", "%s", "%s"\n' % (u'artist_name',u'album_name',u'track_name'))
-for i in random_artists:
-	artist_name = i['artist_name']
-	album_name = i['album_name']
-	track_name = i['track_name']
-	f.write('"%s", "%s", "%s"\n' % (artist_name, album_name, track_name))
-f.close()"""
-
-	playlist = []
-	for a in random_artists:
-		artist_name = fetchArtistInfo(a)['name']
-		album_id = random.choice(fetchAlbumIds(a))
-		album_name = fetchAlbumInfo(album_id)['name']
-		url = "https://api.spotify.com/v1/albums/" + album_id + "/tracks"
-        req = requests.get(url)
-        data = req.json()
-        track_name = random.choice(data['items'])['name']
-        playlist.append((artist_name, album_name, track_name))
-		
-	pd.DataFrame(playlist, columns=['artist_name','album_name','track_name']).to_csv('playlist.csv', index=False, encoding='utf-8')
+	f = open("playlist.csv", "w", encoding = 'utf-8')
+	try:
+		f.write('"%s", "%s", "%s"\n' % (u'artist_name',u'album_name',u'track_name'))
+		for i in playlist:
+			artist_name = i['artist_name']
+			album_name = i['album_name']
+			track_name = i['track_name']
+			f.write('"%s", "%s", "%s"\n' % (artist_name, album_name, track_name))
+	finally:
+		f.close()
